b2c9cd276145e8c2bcb2d1643f23b333adeca39c
Deepa Baby <deepababy91@gmail.com>
2021-10-10 20:31:28 -0500

    checking in

--- readme.txt
+++ readme.txt
@@ -1,3 +1,17 @@
+This is Deepa Baby.This is my capstone project front end part using React JS. 
+My final capstone project is to let users register and login to a forum so that they can view a list of categories, create questions and answers for each category. 
+The main page contains the title of the app, logout link.The logout link will transfer the user back to the login page.
+On the left, there is a menu of the existing categories. 
+Upon selecting a category, the "select a Category to get started" area is replaced by questions.
+When clicking the Create New Question button, user can create a question.
+When  clicking a Question link,user can view its contents .In the question view user can see the details 
+of the question and the list of answers.
+Created the data layer using the  Database management system MySQL. Application layer of this project implemented using node.js and Express.
+Presentation layer implemented using HTML, CSS, Javascript, and the client-side framework using React JS, React Router.
+Heroku is used for the deployment. This is available in different views
+
+
+
 created frontend and backend folders and push to github
 tailwind
 bootstrap
--- src/App.js
+++ src/App.js
@@ -1,193 +1,141 @@
+import logo from './logo.svg';
 import './App.css';
-import React,{useState,useEffect} from 'react';
-import {List,Collapse,Button} from 'antd'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Dashboard from "./dashboard";
+
+import React, {useEffect, useState} from "react";
+
+import {Button, List, Collapse, Breadcrumb, Row, Col, Divider, notification} from 'antd';
 const { Panel } = Collapse;
+
+
+
+function Auth () {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [retypePassword, setRetypePassword] = useState('');
+
+    const onSignIn = async (event) => {
+        event.preventDefault();
+        console.log(email)
+        console.log(password)
+        let res = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({email, password})
+        })
+        let data = await res.json();
+        console.log(data);
+        if(data.token){
+            localStorage.setItem('token', data.token);
+            window.location.href = '/dashboard'
+        }
+
+
+    };
+
+    const onSignUp = async (event) => {
+        event.preventDefault();
+        console.log(email)
+        console.log(password)
+        console.log(retypePassword)
+        let res = await fetch('http://localhost:3000/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({email, password})
+        })
+        let data = await res.json();
+        console.log(data);
+        if(data.success){
+            // Show a success message use ANT design success message?
+            notification['success']({
+                message: 'Thanks',
+                description:
+                    'You are all set. You can now login using your credentials.',
+            });
+
+        }
+
+    };
+
+    return (
+        <div>
+
+            <h1 className={'text-3xl text-center'}>Login Form</h1>
+
+            <form onSubmit={onSignIn}>
+                <Row type={'flex'} align={'center'} className={'mt-5'}>
+                    <Col span={24}>
+                        <input type="email" value={email} onChange={(ev) => setEmail(ev.currentTarget.value)} required className={'border w-full rounded'} placeholder={'Email address'}/>
+                    </Col>
+                    <Col span={24} className={'mt-5'}>
+                        <input type="password" value={password} onChange={(ev) => setPassword(ev.currentTarget.value)} required className={'border w-full rounded'} placeholder={'Password'}/>
+                    </Col>
+                    <Col span={24} className={'mt-5'}>
+                        <Button htmlType={'submit'} className={'border-0 bg-edorble-yellow-500 hover:bg-edorble-yellow-600 hover:text-black w-full rounded font-bold'}>Submit</Button>
+                    </Col>
+                </Row>
+            </form>
+
+
+            <Divider />
+
+            <h1 className={'text-3xl text-center'}>Signup Form</h1>
+
+            <form onSubmit={onSignUp}>
+                <Row type={'flex'} align={'center'} className={'mt-5'}>
+                    <Col span={24}>
+                        <input type="email" value={email} onChange={(ev) => setEmail(ev.currentTarget.value)} required className={'border w-full rounded'} placeholder={'Email address'}/>
+                    </Col>
+                    <Col span={24} className={'mt-5'}>
+                        <input type="password" value={password} onChange={(ev) => setPassword(ev.currentTarget.value)} required className={'border w-full rounded'} placeholder={'Password'}/>
+                    </Col>
+                    <Col span={24} className={'mt-5'}>
+                        <input type="password" value={retypePassword} onChange={(ev) => setRetypePassword(ev.currentTarget.value)} required className={'border w-full rounded'} placeholder={'Retype Password'}/>
+                        {(password != retypePassword) && <small className={'text-red-500 font-bold'}>Passwords don't match</small>}
+                    </Col>
+                    <Col span={24} className={'mt-5'}>
+                        <Button  htmlType={'submit'} disabled={password != retypePassword} className={'border-0 bg-edorble-yellow-500 hover:bg-edorble-yellow-600 hover:text-black w-full rounded font-bold'}>Submit</Button>
+                        {/*<Button loading={loading} disabled={password != retypePassword} type="primary" htmlType={'submit'} className={'border-0 w-full rounded font-bold'}>Submit</Button>*/}
+                    </Col>
+                </Row>
+            </form>
+
+        </div>
+    )
+}
+
 function App() {
-
-  const [categories,setCategories]=useState([])//empty array
-  const [selectedCategory,setSelectedCategory]=useState()
-  const [questions,setQuestions]=useState()
-  const [questionTxt,setQuestionTxt]=useState()
-
-  let apiUrl=process.env.REACT_APP_API_URL || 'http://localhost:3000'
-
-  const fetchCategories=async ()=>{
-    console.log('this will fetch the categories')
-    let res=await fetch (`${apiUrl}/api/v1/categories`)
-    let data=await res.json()
-    console.log(data)
-    setCategories(data)
-    }
-
-  const  fetchQuestionsForCategory =async(id) =>{
-      console.log('fetch questions for this category id ',id)
-      let res=await fetch (`http://localhost:3000/api/v1/categories/${id}/questions`)
-      let data=await res.json()
-      console.log(data)
-      setQuestions(data)
-
-  }
-
-  const createNewQuestion = async ()=>{
-console.log('create a new quetsion for the category id ',selectedCategory)
- let res=await fetch (`${apiUrl}/api/v1/categories/${selectedCategory}/questions`, 
- {method:'POST',
- headers:{
-         'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
- },
- body:JSON.stringify({questionTxt:questionTxt})
-
- })
-fetchQuestionsForCategory(selectedCategory)
-setQuestionTxt('')
-//usual fetch request -POST
-//make a POST request to create a question
-//once the call is successful
-//fetch the questions for a category again (reload the questions)
-//done!
-  }
-  
-
-    /*const fetchQuestions=async ()=>{
-    console.log('this will fetch the questions')
-    let res=await fetch (`http://localhost:3000/api/v1/categories/:categoryId/questions`)
-    let data=await res.json()
-    console.log("questions", data)
-   
-    }*/
-    const createANewAnswer=async ()=>{
- //you will need something called selectedQuestion to keep a track of the question that has been selected 
- //state variable to store the answer text
-//usual fetch request -POST
-//make a POST request to create an answer
-//once the call is successful
-//fetch the questions for a category again (reload the questions)
-//done!
-    }
-
-  useEffect( ()=>{
-    //this code will run only once on component mount
-    fetchCategories();
-
-  },[])
-
-   /*useEffect( ()=>{
-     //this code is going to run whenever the selectedCategory changes
-     fetchQuestions();//fetch and show the questions
-    
-
-  },[selectedCategory])*/
 
 
   return (
-    <>
-   
-    <div className="grid grid-cols-12">
-    <div className ={'col-span-full border p-5'}>
-    <h1 className={'text-center text-3xl'}>Questions App</h1></div>
-    </div>
+      <Router>
+          <div>
 
-    <div className="grid grid-cols-12">
-    <div className ={'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
-   {/* <h1 className={'text-center text-3xl'}>Currently selected category is :{selectedCategory}</h1>*/}
-   {/* <ul>
-    {categories.map((category,index)=>{
-      return<li key={index} className={category.id===selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick = {()=> {
-           setSelectedCategory(category.id)
-           fetchQuestionsForCategory(category.id)
-           
-      }}>
-      
-      {category.name}
-             </li>
-
-    })}
-  </ul>*/}
-
-
-    <List
-      size="Large"
-      header={<div className={'font-bold'}>Categories List</div>}
-      //footer={<div>Footer</div>}
-      bordered
-      dataSource={categories}
-      renderItem={item => <List.Item>
-        
-        <div className={item.id===selectedCategory ? 'cursor-pointer text-blue-500 font-bold' : 'cursor-pointer'} onClick = {()=> {
-           setSelectedCategory(item.id)
-           fetchQuestionsForCategory(item.id)   
-      }}>
-      {item.name}
-      </div>
-      </List.Item>}
-        />
-    </div>
-
-     <div className ={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
-    {/*<h1 className={'text-center text-3xl'}>Blank space</h1>*/}
-
-    {/*<button className={'border p-2 pl-4 pr-4 bg-gray-200'} onClick={createNewQuestion}>New Question</button>*/}
-    {selectedCategory && <div>
-    
-<input value={questionTxt} onChange={(ev)=>{
-      setQuestionTxt(ev.currentTarget.value)
-    }}type="text"  className={'border p-1 mr-5 w-2/3'}/>
-    <Button type={'primary'} onClick={createNewQuestion}>Create new Question</Button>
-    <br/>
-    <br/>
-    </div>}
-    
-    
-   {/* <ul>
-    {questions && questions.map((question)=>{
-     return <li key={question.id}>
-    {question.questionTxt} {question.Answers.length > 0 && <span>-<span>{question.Answers.length}</span></span>}
-     </li>
-    })}
-  </ul>*/}
-
-{selectedCategory && <div>
-<Collapse accordion>
-
-   {questions && questions.map((question,index)=>{
-     return  <Panel header={question.questionTxt} key={index}>
-      <List
-      size="small"
-      //header={<div className={'font-bold'}>Answers List</div>}
-      footer={<div>
-        <input value={questionTxt} onChange={(ev)=>{
-      setQuestionTxt(ev.currentTarget.value)
-    }}type="text"  className={'border p-1 mr-5 w-2/3'}/>
-    <Button type={'primary'} onClick={createANewAnswer}>Add Answer</Button>
-        
-        </div>}
-      bordered
-      dataSource={question.Answers}
-      renderItem={answer => <List.Item>
-        
-        <div>
-      {answer.answerTxt}
-      </div>
-      </List.Item>}
-        />
-    </Panel>
-        })}
-  </Collapse>
-
-</div>}
-{!selectedCategory && <h1 className={'text-center text-xl uppercase tracking-wider text-blue-500'}>Select a Category to get started</h1>}
-   
-
-    
-    {/*if the questions exists show it stringify on the screen
-    {questions && <p>{JSON.stringify(questions)}</p>}*/}
-     
-    </div>
-    </div>
-
-    
-    </>
+              {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+              <Switch>
+                  <Route path="/dashboard">
+                      <Dashboard />
+                  </Route>
+                  <Route path="/">
+                      <Auth />
+                  </Route>
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
